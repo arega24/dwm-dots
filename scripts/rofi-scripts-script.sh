505ddda8841497a7f76d1a401ecaffac8c1780be
arega24 <dfbarega@gmail.com>
2023-02-25 17:57:07 +0000

    iwd script

--- scripts/rofi-scripts-script.sh
+++ scripts/rofi-scripts-script.sh
@@ -6,10 +6,10 @@ rofi_command="rofi -demenu -i -theme /home/arega/.config/rofi/scripts-script.ras
 one="USB's" 
 too="Mixer" 
 three="Calculator" 
-#four="" 
+four="Wifi" 
 
 # Variable passed to rofi 
-options="$one\n$too\n$three"   
+options="$one\n$four\n$too\n$three"   
 
 case "$(echo "$options" | $rofi_command -dmenu -selected-row 2)" in     
 	$one)         
@@ -21,7 +21,7 @@ case "$(echo "$options" | $rofi_command -dmenu -selected-row 2)" in
 	$three)         
 		rofi -show calc -no-show-match -no-sort -display-calc "Calc: " 		
 		;;     
-#	$four)         
-#		
-#		;;     
+	$four)         
+	    ~/.config/scripts/rofi-wifi-menu-iwd.sh	
+		;;     
 esac
--- scripts/set-wallpaper.sh
+++ scripts/set-wallpaper.sh
@@ -2,7 +2,7 @@
 
 wal -n -i $1 &
 feh --bg-scale $1
-sh $HOME/.config/zathura/Zathura-Pywal/dirzathurarc &
+sh $HOME/.config/zathura/zathura-pywal/dirzathurarc &
 sh $HOME/.config/dunst/dunst-pywal/dirdunstrc &
 
 dwmc reloadxrdb &
